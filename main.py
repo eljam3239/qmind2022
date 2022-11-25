9991d6196345bfc95336d8b986bf277a81eb5fb2
eljam3239 <elijames2301@gmail.com>
2022-11-24 20:45:12 -0500

    Add todos for people to take a stab at

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-if __name__ == '__main__':
-    print('Hey ')
+import sys
+import sklearn
+import numpy as np
+import matplotlib as mpl
+import pandas as pd
+import os
+import tarfile
+import urllib.request
+data = pd.read_csv('data/diabetes_binary_health_indicators_BRFSS2015.csv')
+
+#print(data.head)
+#print(data.info)
+#print(data.describe)
